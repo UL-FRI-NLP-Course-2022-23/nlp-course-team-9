c7bc9f2095336f7a460f9041fbef162b24833d77
Martin Bavčar <m@bavcar.si>
2023-05-01 10:09:02 +0200

    added stuff before moving to hpc

---
+++ evaluation.py
@@ -0,0 +1,35 @@
+import os
+import pickle as pkl
+import random
+from pprint import pprint
+
+# Parameters
+random.seed(1)
+base_dir = 'data/back_translations/'
+
+
+def merge_pkls():
+    all_paragraphs = []
+
+    for f in os.listdir(base_dir):
+        f_split = f.split('.')
+        if len(f_split) == 2:
+            filename, extension = f_split
+            if extension == 'pkl':
+                with open(base_dir + f, 'rb') as pkl_file:
+                    pkl_paragraphs = pkl.load(pkl_file)
+                    all_paragraphs += pkl_paragraphs
+
+    with open(base_dir + '3rd_try.pkl', 'wb') as pkl_file:
+        pkl.dump(all_paragraphs, pkl_file)
+
+
+if __name__ == '__main__':
+    all_paragraphs = dict()
+    with open(base_dir + '3rd_try.pkl', 'rb') as pkl_file:
+        for i, (original, back_translation) in enumerate(pkl.load(pkl_file)):
+            all_paragraphs[i] = (original, back_translation)
+
+    print(f'{len(all_paragraphs.values())} paragraphs in total.')
+
+    pprint(random.sample(all_paragraphs.items(), 10), width=210)
