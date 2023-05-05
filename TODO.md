c3ab7f28ce7697246dc1eb8232d434d526365d82
Martin Bavčar <m@bavcar.si>
2023-05-05 21:34:30 +0200

    hpc tg fix

--- TODO.md
+++ TODO.md
@@ -3,5 +3,4 @@
 - ročno evalvirat podatke iz back-translationa
 - na koncu pognati na testnih podatkih
 - izbrati kako velik model uporabiti ali pa kakšnega drugega (https://www.kdnuggets.com/2023/02/simple-nlp-pipelines-huggingface-transformers.html)
-- shraniti natreniran model na koncu
 - mogoče mal povečat learning rate pa dodat nek weight_decay (https://pytorch.org/docs/stable/generated/torch.optim.Adam.html)
--- slurm/tg_status.py
+++ slurm/tg_status.py
@@ -14,12 +14,13 @@ def send_status(info_msg):
     Upon job submission, a message is sent to the Telegram channel specified in
     tg_credentials.txt file.
     """
-    with open("tg_credentials.txt") as credentials_file:
-        bot_token, channel_token = credentials_file.readline().split()
-
-    with requests.session() as s:
-        a = s.post("https://api.telegram.org/bot" + bot_token + "/sendMessage?chat_id=-100" + channel_token[1:] + "&text=" + info_msg)
-        return True
+    if os.path.isfile("tg_credentials.txt"):
+        with open("tg_credentials.txt") as credentials_file:
+            bot_token, channel_token = credentials_file.readline().split()
+    
+        with requests.session() as s:
+            a = s.post("https://api.telegram.org/bot" + bot_token + "/sendMessage?chat_id=-100" + channel_token[1:] + "&text=" + info_msg)
+            return True
 
 
 if __name__ == "__main__":
